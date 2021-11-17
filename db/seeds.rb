f02929e1f06fa9e7ec3e1417c756903955cbf872
Aymeric Erulin <erulin.aymeric@gmail.com>
2021-11-17 16:15:34 +0100

    Revert "rails new"
    This reverts commit 0c596a0208ac494f5f26dd65639d9d200734ceb3.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Creating 50 articles...'
+
+50.times do
+  article = Article.new(
+    title: Faker::Book.title,
+    content: Faker::Lorem.paragraph(sentence_count: 5),
+    image: "https://unsplash.it/400/400?image=#{rand(500)}"
+  )
+  article.save!
+end
+
+puts 'All articles have been created!'
